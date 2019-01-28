121e7e196acd14a1cd7abd7c2f3778a7125a69ca
nimrodbu7 <nimibu76?>
2019-01-28 15:52:35 +0200

    complete main script working, both algorithms supported;

--- src/main/main.py
+++ src/main/main.py
@@ -2,7 +2,12 @@
 import sys, getopt
 import json
 import os
-from pprint import pprint
+
+from simple_log_parser import SimpleLogParser
+import src.statistical_diffs.statistical_log_diff_analyzer as snk
+import src.statistical_diffs.statistical_log_diff_analyzer as s2k
+import pandas as pd
+
 
 def process_input_files(argv):
     '''
@@ -14,9 +19,9 @@ def process_input_files(argv):
     alpha = ''
     delta = ''
     try:
-        opts, args = getopt.getopt(argv, "ha:d:c:", ["input_config=", "alpha=", "delta="])
+        opts, args = getopt.getopt(argv, "ha:d:c:k:", ["input_config=", "alpha=", "delta=", "k_param="])
     except getopt.GetoptError:
-        print('error parsing input parameters, call: test.py -c <input_config> -a <alpha> -d <delta>')
+        print('error parsing input parameters, call: test.py -c <input_config> -a <alpha> -d <delta> -k <k_param>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -25,9 +30,20 @@ def process_input_files(argv):
         elif opt in ("-c", "--config"):
             inputfile = arg
         elif opt in ("-a", "--alpha"):
-            alpha = arg
+            try:
+                alpha = float(arg)
+            except:
+                raise ValueError('alpha must be a float')
         elif opt in ("-d", "--delta"):
-            delta = arg
+            try:
+                delta = float(arg)
+            except:
+                raise ValueError('delta must be a float')
+        elif opt in ("-k", "--k_param"):
+            try:
+                k = int(arg)
+            except:
+                raise ValueError('k must be a integer')
 
     with open(inputfile) as f:
         data = json.load(f)
@@ -54,18 +70,63 @@ def process_input_files(argv):
         raise ValueError('output_dir does missing', output_dir)
     if not os.path.isdir(output_dir):
         raise ValueError('output_dir does map to a dir', output_dir)
-    return log_paths, output_dir, alpha, delta
+    return log_paths, output_dir, alpha, delta, k
+
+def run(log_paths, output_dir, alpha, delta, k):
+
+    def handle_multiple_logs_mapping(diff, logs_ids):
+        ## map diffs to log indices
+        if 'experiments_per_log' in diff:
+            item['experiments_per_log'] = dict(
+                [(logs_ids[i], diff['experiments_per_log'][i]) for i in
+                 range(len(diff['experiments_per_log']))])
+
+        ## map difference pairs to log indices
+        if 'different_ids' in item:
+            diff_tuples = diff['different_ids']
+            item['different_ids'] = [(logs_ids[diff_tuples[i][0]], logs_ids[diff_tuples[i][1]]) for i in
+                                            range(len(diff_tuples))]
+        if 'significant_diffs' in item:
+            pairwise_diffs = item['significant_diffs']
+            new_pairwise_diffs = {}
+            for pair in pairwise_diffs:
+                pair_with_log_ids = (logs_ids[pair[0]], logs_ids[pair[1]])
+                new_pairwise_diffs[pair_with_log_ids] = \
+                    pairwise_diffs[pair]
+            item['significant_diffs'] = new_pairwise_diffs
+
+    logs = {}
+    for log in log_paths:
+        logs[log] = SimpleLogParser.read_log(log_paths[log])
+    logs_ids = list(logs.keys())
+
+    logs_arr = list(logs.values())
+    alg = snk.MultipleSLPDAnalyzer(logs_arr) if len(logs) > 2 else s2k.SLPDAnalyzer(logs_arr[0], logs_arr[1])
+    diffs = alg.find_statistical_diffs(k, delta, alpha)
+    vals = "_".join(['k_' + str(k), 'd_' + str(delta), 'al_' + str(alpha)])
+    keys = list(diffs[list(diffs.keys())[0]].keys())
+    keys.extend(['source', 'target'])
+    df = pd.DataFrame(columns=keys)
+    for diff in diffs:
+        item = diffs[diff].copy()
+        handle_multiple_logs_mapping(diffs[diff], logs_ids)
+        item['source'] = diff[0]
+        item['target'] = diff[1]
+        df = df.append(item, ignore_index=True)
+    df.to_csv(output_dir + 'results' + "_" + vals + '_diffs' + '.csv')
 
 def main(argv):
 
-    log_paths, output_dir, alpha, delta = process_input_files(argv)
+    log_paths, output_dir, alpha, delta, k = process_input_files(argv)
     print('Input log files:')
     for log in log_paths:
         print(log, ':', log_paths[log])
     print('Output dir:', output_dir)
     print('Alpha =', alpha)
     print('Delta =', delta)
+    print('k-parameter =', k)
 
+    run(log_paths, output_dir, alpha, delta, k)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
