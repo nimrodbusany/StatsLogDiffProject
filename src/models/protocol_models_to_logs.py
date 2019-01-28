534708402c9ead8262bcdb85a37c0cbaf8b81a2f
nimrodbu7 <nimibu76?>
2019-01-28 13:58:35 +0200

    added parsing of Davids model

--- src/models/protocol_models_to_logs.py
+++ src/models/protocol_models_to_logs.py
@@ -4,14 +4,14 @@ import networkx as nx
 import os
 from model_based_log_generator import LogGeneator
 from log_writer import LogWriter
-
+from logs_parsers.simple_log_parser import SimpleLogParser
+import numpy as np
 
 TRANSITION_PROBABILITY_ATTRIBUTE = 'transition_probability'
 TRANSITION_LABEL_ATTRIBUTE = 'label'
 
 
 class ProtocolModel:
-
 
     @staticmethod
     def _add_sink_node(graph):
@@ -111,18 +111,78 @@ def produce_logs_from_stamina(traces2produce):
             model_generator.write_transitions_probabilities(dir_)
             LogWriter.write_log(log, dir_ + 'l' + str(instance_id) + ".log")
 
-def produce_logs_from_padel(): ## TODO
-    pass
 
-def produce_logs_from_david(): ## TODO
-    pass
+def produce_logs_from_david(traces2produce):
+    MODELS_PATH = '../../models/david/'
+    LOGS_OUTPUT_PATH = '../../data/logs/david/'
+    models = ['Columba.simplified.dot', 'Heretix.simplified.dot', 'JArgs.simplified.dot',
+              'Jeti.Simplified.dot', 'jfreechart.Simplified.dot', 'OpenHospital.Simplified.dot',
+              'RapidMiner.Simplified.dot', 'tagsoup.Simplified.dot']
+    dirs_ = ['Columba', 'Heretix', 'JArgs', 'Jeti', 'jfreechart', 'OpenHospital', 'RapidMiner', 'tagsoup']
+    MODEL_TO_PRODUCE = 4
+
+    for model_id in range(0, len(models)):
+        print('processing', models[model_id])
+        dir_ = LOGS_OUTPUT_PATH + dirs_[model_id] + "/"
+        model_path = MODELS_PATH + models[model_id]
+        if not os.path.exists(dir_):
+            os.makedirs(dir_)
+
+        for instance_id in range(MODEL_TO_PRODUCE):
+            print('processing instance:', instance_id, model_path)
+            model_generator = ProtocolModel(model_path, instance_id, assign_transtion_probs=True)
+            # transition_probs = nx.get_edge_attributes(model_generator.graph.dgraph, TRANSITION_PROBABILITY_ATTRIBUTE)
+            # for e in transition_probs:
+            #     from fractions import Fraction
+            #     transition_probs[e] = float(Fraction(transition_probs[e].strip("\"")))
+            # nx.set_edge_attributes(model_generator.graph.dgraph, transition_probs, TRANSITION_PROBABILITY_ATTRIBUTE)
+
+            log = LogGeneator.produce_log_from_model(model_generator.graph,
+                                                     transition_probability_attribute=TRANSITION_PROBABILITY_ATTRIBUTE,
+                                                     traces2produce=traces2produce)
+            model_generator.write_transitions_probabilities(dir_)
+            LogWriter.write_log(log, dir_ + 'l' + str(instance_id) + ".log")
+
+
+
+def compute_logs_statistics(logs_base_folder, dirs_, logs_in_folder):
+    for dir_ in dirs_:
+        model_means_ = []
+        for instance_id in range(logs_in_folder):
+            log_path = logs_base_folder + dir_ + "/l" + str(instance_id) + ".log"
+            traces = SimpleLogParser.read_log(log_path)
+            model_means_.append(np.mean([len(t) for t in traces]))
+        print(dir_, np.mean(model_means_))
+
+def compute_bear_stats():
+
+    LOGS_OUTPUT_PATH = '../../data/bear/'
+    dirs_ = ['mobile.log', 'desktop.log']
+    traces = SimpleLogParser.read_log(LOGS_OUTPUT_PATH + dirs_[0])
+    print('mobile', np.mean([len(t) for t in traces]))
+    print('mobile alphabet', len(set([w for t in traces for w in t])))
+    traces = SimpleLogParser.read_log(LOGS_OUTPUT_PATH + dirs_[1])
+    print('desktop', np.mean([len(t) for t in traces]))
+    print('desktop alphabet', len(set([w for t in traces for w in t])))
 
 
 if __name__ == '__main__':
 
     TRACE2PRODUCE = 100000
-    produce_logs_from_stamina(TRACE2PRODUCE)
-    produce_logs_from_padel()
-    produce_logs_from_david()
+    LOGS_IN_FOLDER = 4
+
+    # produce_logs_from_stamina(TRACE2PRODUCE)
+    produce_logs_from_david(TRACE2PRODUCE)
+    #
+    # LOGS_OUTPUT_PATH = '../../data/logs/david/'
+    # dirs_ = ['Columba', 'Heretix', 'JArgs', 'Jeti', 'jfreechart', 'OpenHospital', 'RapidMiner', 'tagsoup']
+    # compute_logs_statistics(LOGS_OUTPUT_PATH, dirs_, LOGS_IN_FOLDER)
+
+    # compute_bear_stats()
+
+
+    # LOGS_OUTPUT_PATH = '../../data/logs/stamina/'
+    # dirs_ = ['ctas.net', 'cvs.net', 'roomcontroller', 'ssh', 'tcpip', 'zip']
+    # compute_logs_statistics(LOGS_OUTPUT_PATH, dirs_, LOGS_IN_FOLDER)
 
 
