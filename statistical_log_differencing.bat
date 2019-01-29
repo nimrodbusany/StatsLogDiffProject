5757f650f23a4a6bdf4a6cbadbd2962c16cdb7a8
nimrodbu7 <nimibu76?>
2019-01-29 15:52:26 +0200

    changed json file names

--- statistical_log_differencing.bat
+++ statistical_log_differencing.bat
@@ -1,3 +1,3 @@
 echo %PYTHONPATH%
 set PYTHONPATH=C:\Users\USER\PycharmProjects\StatsLogDiffProject
-python "src/main/main.py" -c input_config_pair.json -a 0.05 -d 0.05 -k 2
+python "src/main/main.py" -c input_config_s2kdiff.json -a 0.05 -d 0.05 -k 2
