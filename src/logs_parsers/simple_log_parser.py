937e29df4d077fbe092b525ebc2c2af9b7af5d58
nimrodbu7 <nimibu76?>
2019-02-14 12:52:55 +0200

    Added validation print

--- src/logs_parsers/simple_log_parser.py
+++ src/logs_parsers/simple_log_parser.py
@@ -10,11 +10,14 @@ class SimpleLogParser:
         with open(log_path) as fr:
             lines = [l.strip() for l in fr.readlines()]
             tr = []
+            i = 0
             for l in lines:
-                if l == SimpleLogParser.TRACE_SEPARATOR:
+                if l.strip() == SimpleLogParser.TRACE_SEPARATOR:
                     if len(tr) > 0:
                         traces.append(tr)
                         tr = []
                     continue
                 tr.append(l)
+                i+=1
+        print('-> Done parsing ', log_path, 'read', len(traces), 'traces')
         return traces
