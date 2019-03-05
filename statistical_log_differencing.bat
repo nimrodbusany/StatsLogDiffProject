0c418f8a0b60c6823186f7057852e815a00e9c9a
nimrodbu7 <nimibu76?>
2019-03-05 11:41:05 +0200

    added configurations, improved bat script

--- statistical_log_differencing.bat
+++ statistical_log_differencing.bat
@@ -1,3 +1,3 @@
 echo %PYTHONPATH%
-set PYTHONPATH=C:\Users\USER\PycharmProjects\StatsLogDiffProject
+set PYTHONPATH=%~dp0
 python "src/main/log_diff_runner.py" -c paper_examples/configurations/input_config_s2kdiff_case_study_2.json -a 0.05 -d 0.2 -k 1 -r 2
